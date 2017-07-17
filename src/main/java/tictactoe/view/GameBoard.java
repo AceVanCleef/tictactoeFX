1a2757e7cbdbbba77f144ce9d36ceb0301560635
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-07-17 17:54:14 +0200

    view: - GameBoard generated.

--- src/main/java/tictactoe/view/GameBoard.java
+++ src/main/java/tictactoe/view/GameBoard.java
@@ -1,18 +1,26 @@
 package tictactoe.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.layout.GridPane;
 import tictactoe.presentationmodel.RootPM;
 import tictactoe.view.components.BoardField;
 import tictactoe.view.util.ViewMixin;
 
 /**
+ * A game board with a dimension of 3 x 3 fields.
+ *
  * Created by degonas on 17.07.2017.
  */
 public class GameBoard extends GridPane implements ViewMixin {
 
     private final RootPM pm;
 
-    private BoardField field;
+    /* the maximum amount of fields this gameboard has. */
+    private static int AMOUNT_OF_FIELDS = 9;
+
+    /* holds all fields of this gameboard. */
+    private ObservableList<BoardField> allFields = FXCollections.observableArrayList();
 
     public GameBoard(RootPM pm){
         this.pm = pm;
@@ -21,11 +29,22 @@ public class GameBoard extends GridPane implements ViewMixin {
 
     @Override
     public void initializeParts() {
-        field = new BoardField(pm);
+        for (int i = 0; i < AMOUNT_OF_FIELDS; ++i) {
+            allFields.add(new BoardField(pm));
+        }
     }
 
     @Override
     public void layoutParts() {
-        add(field, 0, 0);
+
+        //fill GameBoard with n x n fields. n = rowLength.
+        int i = 0, rowIndex = 0, rowLenght = (int) Math.sqrt(AMOUNT_OF_FIELDS);
+        for(BoardField field : allFields){
+            add(field, i % rowLenght, rowIndex);
+            i++;
+            if (i % rowLenght == 0){    //new line
+                rowIndex++;
+            }
+        }
     }
 }
