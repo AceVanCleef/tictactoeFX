9d6851cccec8ab19dba3913f5842a07c09123d88
Stefan Wohlgensinger <stefan.wohlgensinger@students.fhnw.ch>
2017-07-18 12:29:13 +0200

    PM: - PlayerPM - Working on RootPM

--- src/main/java/tictactoe/presentationmodel/RootPM.java
+++ src/main/java/tictactoe/presentationmodel/RootPM.java
@@ -1,5 +1,7 @@
 package tictactoe.presentationmodel;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -12,15 +14,36 @@ public class RootPM {
     /* the maximum amount of fields this gameboard has. */
     public final static int AMOUNT_OF_FIELDS = 9;
 
-    private ObservableList<BoardFieldPM> allFields = FXCollections.observableArrayList();
+    private final ObservableList<BoardFieldPM> allFields = FXCollections.observableArrayList();
 
-
+    private final ObservableList<PlayerPM> allPlayers = FXCollections.observableArrayList();
+    private final IntegerProperty currentPlayerId = new SimpleIntegerProperty();
 
     public RootPM(){
         //prepare the GameBoard
         for (int i = 0; i < AMOUNT_OF_FIELDS; ++i) {
             allFields.add(new BoardFieldPM(i));
         }
+
+        //prepare playerPMs
+        allPlayers.addAll(new PlayerPM(1), new PlayerPM(2));
+
+        //this player begins the match
+        setCurrentPlayerId(allPlayers.get(0).getId());
     }
 
+
+    /******************************* getters and setters *********************************/
+
+    public int getCurrentPlayerId() {
+        return currentPlayerId.get();
+    }
+
+    public IntegerProperty currentPlayerIdProperty() {
+        return currentPlayerId;
+    }
+
+    public void setCurrentPlayerId(int currentPlayerId) {
+        this.currentPlayerId.set(currentPlayerId);
+    }
 }
